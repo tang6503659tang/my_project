387d4a6bf456cd252d58c3e68b9e35c309e76bc9
唐鸿林 <1355879370@qq.com>
2020-04-20 19:00:16 +0800

    前端架构先省略

--- MyFirstPythonProject/www/coroweb.py
+++ MyFirstPythonProject/www/coroweb.py
@@ -121,53 +121,56 @@ class RequestHandler(object):
         self._has_named_kw_arg=has_named_kw_arg(fn)
         self._has_var_kw_arg=has_var_kw_arg(fn)
         self._has_request_arg=has_request_arg(fn)
-    async def __call__(self, request):
+
+    @asyncio.coroutine
+    def __call__(self, request):
         kw = None
-        if self._has_var_kw_arg or self._has_named_kw_arg or self._has_request_arg:
-            if request.method=='post':  # use post method
+        if self._has_var_kw_arg or self._has_named_kw_arg or self._required_kw_args:
+            if request.method == 'POST':
                 if not request.content_type:
-                    return web.HTTPBadRequest("Missing Content-Type!") # data form
-                ct=request.content_type.lower()
+                    return web.HTTPBadRequest('Missing Content-Type.')
+                ct = request.content_type.lower()
                 if ct.startswith('application/json'):
-                    params = await request.json()
-                    if not isinstance(params,dict):
-                        return web.HTTPBadRequest("JSON body must be object!")
+                    params = yield from request.json()
+                    if not isinstance(params, dict):
+                        return web.HTTPBadRequest('JSON body must be object.')
                     kw = params
                 elif ct.startswith('application/x-www-form-urlencoded') or ct.startswith('multipart/form-data'):
-                    params = await request.post()
+                    params = yield from request.post()
                     kw = dict(**params)
                 else:
-                    return web.HTTPBadRequest("Unsupported Content-Type: %s" % request.content_type)
-            if request.method == 'get':
+                    return web.HTTPBadRequest('Unsupported Content-Type: %s' % request.content_type)
+            if request.method == 'GET':
                 qs = request.query_string
                 if qs:
-                    kw=dict()
-                    for k,v in parse.parse_qs(qs,True).items():
+                    kw = dict()
+                    for k, v in parse.parse_qs(qs, True).items():
                         kw[k] = v[0]
-            if kw is None:
-                kw=dict(**request.match_info)
-            else:
-                if not self._has_var_kw_arg and self._has_named_kw_arg:
-                    copy = dict()
-                    for name in self._named_kw_args:
-                        if name in kw:
-                             copy[name]=kw[name]
-                    kw=copy # remove all unnamed kw
-
-                # check named arg
-                for k,v in request.match_info.items():
-                    if k in kw:
-                        logging.warning('Duplicate arg name in named args and kw args: %s' % k)
-                    kw[k]=v
-            if self._has_request_arg:
-                kw['request']=request
-            if self._required_kw_args:
-                for name in self._required_kw_args:
-                    if not name in kw:
-                        return web.HTTPBadRequest("Missing argument: %s" % name)
-            logging.info('Call with args: %s' % str(kw))
-            try:
-                r = await self._func(**kw)
-            except APIError as e:
-                return dict(error=e.error,data=e.data,message=e.message)
-
+        if kw is None:
+            kw = dict(**request.match_info)
+        else:
+            if not self._has_var_kw_arg and self._named_kw_args:
+                # remove all unamed kw:
+                copy = dict()
+                for name in self._named_kw_args:
+                    if name in kw:
+                        copy[name] = kw[name]
+                kw = copy
+            # check named arg:
+            for k, v in request.match_info.items():
+                if k in kw:
+                    logging.warning('Duplicate arg name in named arg and kw args: %s' % k)
+                kw[k] = v
+        if self._has_request_arg:
+            kw['request'] = request
+        # check required kw:
+        if self._required_kw_args:
+            for name in self._required_kw_args:
+                if not name in kw:
+                    return web.HTTPBadRequest('Missing argument: %s' % name)
+        logging.info('call with args: %s' % str(kw))
+        try:
+            r = yield from self._func(**kw)
+            return r
+        except APIError as e:
+            return dict(error=e.error, data=e.data, message=e.message)
